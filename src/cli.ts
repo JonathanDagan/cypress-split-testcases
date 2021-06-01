f8a207fcd3b4f04b3755460a48e5ee3ed8e570b5
Jonathan <jonathandagan12@gmail.com>
2021-06-01 21:19:15 +0300

    wip

--- src/cli.ts
+++ src/cli.ts
@@ -4,12 +4,21 @@ let options = { a: "a" };
 
 
 (async () => {
-    var promt = inquirer.createPromptModule()
-    promt({
-        type: "list",
-        message: "Pick the option you're using:",
-        name: "option",
-        choices: Object.keys(options)
-    })
+    // inquirer.prompt([])
+    var { awnser } = await inquirer.prompt([
+        {
+            type: "list",
+            message: "Pick the option you're using:",
+            name: "option",
+            choices: Object.keys(options)
+        }
+    ])
+    // var promt = inquirer.createPromptModule()
+    // promt({
+    //     type: "list",
+    //     message: "Pick the option you're using:",
+    //     name: "option",
+    //     choices: Object.keys(options)
+    // })
 
-})();
+})()
